de06a9924e0d7ab597aad4b7e0d867b52a4d7056
fabianchs <fabichasola@hotmail.com>
2021-05-24 20:00:08 +0000

    package.json change

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
+import { Login } from "./pages/login";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Button } from "reactstrap";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="text-center m-5">
+			<div className="row d-flex justify-content-center mt-5">
+				<div className="row mb-3 d-flex align-items-center justify-content-center">
+					<h1 id="letters" className="m-2">
+						&#128513; ¡UTÚ! De un estudiante para futuros estudiantes.
+					</h1>
+					<Button color="success" className="m-2 rounded-pill">
+						Registrarme!
+					</Button>{" "}
+				</div>
+				<div className="row d-flex justify-content-center">
+					<div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+						{" "}
+						<div className="card m-1 shadow">
+							<h5 className="card-title m-1">Aprendizaje</h5>
+							<img
+								src="https://s1.1zoom.me/prev/579/Abstraction_Vector_Graphics_Texture_578280_600x400.jpg"
+								className="card-img-top pt-1"
+								alt="..."
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Utú es un banco de preguntas para las pruebas de aptitud académica de las
+									universidades públicas de Costa Rica. &#127919;
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+						<div className="card m-1 shadow">
+							<h5 className="card-title m-1">Práctica</h5>
+							<img
+								src="https://s1.1zoom.me/prev/579/Abstraction_Vector_Graphics_Texture_578280_600x400.jpg"
+								className="card-img-top pt-1"
+								alt="..."
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Utú es una plataforma gratuita, creada con el fin de ofrecer a los costarricenses un
+									medio de práctica para las pruebas. &#11088;
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+						<div className="card m-1 shadow">
+							<h5 className="card-title m-1">Mejora</h5>
+							<img
+								src="https://s1.1zoom.me/prev/579/Abstraction_Vector_Graphics_Texture_578280_600x400.jpg"
+								className="card-img-top pt-1"
+								alt="..."
+							/>
+							<div className="card-body">
+								<p className="card-text">
+									Utú está trabajando para ofrecer a futuro las explicaciones de los enunciados dentro
+									de la plataforma. &#128151;
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
