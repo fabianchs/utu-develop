490aa7f25859e89e103358e7a60bddbbb3967f73
fabianchs <fabichasola@hotmail.com>
2021-06-06 00:05:58 +0000

    working

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-warning mb-3 fixed-top bg-warning">
+		<nav className="navbar navbar-warning mb-3 fixed-top bg-warning d-flex align-items-center">
 			<Link to="/">
-				<img src="https://res.cloudinary.com/dubb4luoi/image/upload/v1622926480/android-icon-36x36_elru6i.png" />
+				<h3 className="text-light d-flex align-items-center">
+					<img src="https://res.cloudinary.com/dubb4luoi/image/upload/v1622926480/android-icon-36x36_elru6i.png" />
+					UTÚ
+				</h3>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
