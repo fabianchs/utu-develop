36960f72931b320f08448a25b58b19b0815d9f1a
fabianchs <fabichasola@hotmail.com>
2021-06-11 16:16:47 +0000

    resposive navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,7 +21,7 @@ export const Nav_bar = () => {
 	const { store, actions } = useContext(Context);
 
 	const [isOpen, setIsOpen] = useState(false);
-	const [navItems, setNavItems] = useState("");
+	//const [navItems, setNavItems] = useState(""); //REVISAR PARA BORRAR
 	const toggle = () => setIsOpen(!isOpen);
 
 	const closeIfOpen = () => {
@@ -119,12 +119,12 @@ export const Nav_bar = () => {
 		}
 	}
 
-	useEffect(
-		() => {
-			setNavItems(conditionalNavItems());
-		},
-		[store.token, store.isUser, store.isAdmin, store.isBoss]
-	);
+	// useEffect(//REVISAR PARA BORRAR POSTERIOR
+	// 	() => {
+	// 		setNavItems(conditionalNavItems());
+	// 	},
+	// 	[store.token, store.isUser, store.isAdmin, store.isBoss]
+	// );
 
 	return (
 		<Navbar color="success" light expand="md" className="fixed-top">
@@ -145,50 +145,5 @@ export const Nav_bar = () => {
 				{conditionalNavItems()}
 			</Collapse>
 		</Navbar>
-
-		// <nav className="navbar navbar-expand-lg navbar-light bg-success fixed-top">
-		// 	<div className="container">
-		// 		<div className="navbar-brand">
-		// <Link to="/">
-		// 	<span className="h2 d-flex align-items-center text-light ">
-		// 		<img
-		// 			height="auto "
-		// 			src="https://res.cloudinary.com/dubb4luoi/image/upload/v1622926480/android-icon-36x36_elru6i.png"
-		// 		/>
-		// 		UTÚ
-		// 	</span>
-		// </Link>
-		// 		</div>
-		// 		<button
-		// 			type="button"
-		// 			className="navbar-toggler"
-		// 			data-toggle="collapse"
-		// 			data-target="#principal-menu"
-		// 			aria-controls="principal-menu"
-		// 			aria-expanded="false"
-		// 			aria-label="Desplegar menú de navegación">
-		// 			<span className="navbar-toggler-icon" />
-		// 		</button>
-		// 		<div className="collapse navbar-collapse" id="principal-menu">
-		// 			<ul className="navbar-nav">
-		// 				<li className="nav-item">
-		// <Link to="/practice" className="nav-link text-light h3">
-		// 	Práctica
-		// </Link>
-		// 				</li>
-		// 				<li className="nav-item">
-		// <Link to="/login" className="nav-link text-light h3">
-		// 	Ingresar
-		// </Link>
-		// 				</li>
-		// 				<li className="nav-item">
-		// <Link to="/register" className="nav-link text-light h3">
-		// 	Registrarme
-		// </Link>
-		// 				</li>
-		// 			</ul>
-		// 		</div>
-		// 	</div>
-		// </nav>
 	);
 };
