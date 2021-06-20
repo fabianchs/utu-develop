239db03cf5031a1a8e52e8bd98af1ddd330e8939
fabianchs <fabichasola@hotmail.com>
2021-06-20 18:06:00 +0000

    prev list, space

--- src/front/js/admin_pages/admin_create.js
+++ src/front/js/admin_pages/admin_create.js
@@ -16,27 +16,79 @@ export const AdminCreate = () => {
 	//const statement = ["Hola!", "(a^2+3)/56", "prueba", "\\dfrac{a^2+3}{56}", "x^2-56"];
 	//const types = ["t", "f", "t", "f", "f"];
 
+	let text = (
+		<div className="row p-1 pt-0 border rounded-1 shadow mt-1">
+			<div className="col-12 m-0 p-0 d-flex justify-content-between">
+				<div className="m-0 p-0">
+					<Badge color="secondary">Actual: Texto</Badge>
+					<Badge color="dark">Texto</Badge>
+					<Badge color="dark">Fórmula</Badge>
+					<Badge color="dark">Imagen</Badge>
+					<Badge color="dark">Separador</Badge>
+					<Badge color="dark">Lista</Badge>
+				</div>
+				<div className="float-end">
+					<Badge color="danger">X</Badge>
+				</div>
+			</div>
+			<Input type="textarea" name="text" id="exampleText" />
+		</div>
+	);
+	let list = (
+		<div className="row p-1 pt-0 border rounded-1 shadow mt-3">
+			<div className="col-12 m-0 p-0 d-flex justify-content-between">
+				<div className="m-0 p-0">
+					<Badge color="secondary">Actual: Lista</Badge>
+					<Badge color="dark">Texto</Badge>
+					<Badge color="dark">Fórmula</Badge>
+					<Badge color="dark">Imagen</Badge>
+					<Badge color="dark">Separador</Badge>
+					<Badge color="dark">Lista</Badge>
+				</div>
+				<div className="float-end">
+					<Badge color="danger">X</Badge>
+				</div>
+			</div>
+			<Input type="text" name="text" id="exampleText" className="mb-1" />
+			<Input type="text" name="text" id="exampleText" className="mb-1" />
+			<Input type="text" name="text" id="exampleText" className="mb-1" />
+			<div className="col-12 d-flex justify-content-end">
+				<Button color="secondary" size="sm" className="p-0">
+					<p className="m-0 p-0">
+						&nbsp;
+						<i className="fas fa-plus" />
+						&nbsp;
+					</p>
+				</Button>
+				&nbsp;
+				<Button color="secondary" size="sm" className="p-0">
+					<p className="m-0 p-0">
+						&nbsp;
+						<i className="fas fa-minus" />
+						&nbsp;
+					</p>
+				</Button>
+			</div>
+		</div>
+	);
+
+	let space = (
+		<div className="row p-1 pt-0 border rounded-1 shadow mt-3 bg-secondary">
+			<Badge color="secondary" className="col-12 d-flex justify-content-between bg-secondary text-dark">
+				SEPARADOR
+				<Badge color="danger">X</Badge>
+			</Badge>
+		</div>
+	);
+
 	return (
 		// <div className="mx-auto pt-5">
 		<div className="container-fluid mt-5 pt-5">
 			<div className="row m-2">
 				<div className="col-6">
-					<div className="row p-1 pt-0 border rounded-1 shadow">
-						<div className="col-12 m-0 p-0 d-flex justify-content-between">
-							<div className="m-0 p-0">
-								<Badge color="secondary">Actual: Texto</Badge>
-								<Badge color="dark">Texto</Badge>
-								<Badge color="dark">Fórmula</Badge>
-								<Badge color="dark">Imagen</Badge>
-								<Badge color="dark">Separador</Badge>
-								<Badge color="dark">Lista</Badge>
-							</div>
-							<div className="float-end">
-								<Badge color="danger">X</Badge>
-							</div>
-						</div>
-						<Input type="textarea" name="text" id="exampleText" />
-					</div>
+					{text}
+					{list}
+					{space}
 				</div>
 
 				<div className="col-6">sadf</div>
