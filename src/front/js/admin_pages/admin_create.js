bfb9ce0c32a76720881a9fdbc14cff700f0d1cd3
fabianchs <fabichasola@hotmail.com>
2021-06-23 20:22:07 +0000

    working on create/buttons

--- src/front/js/admin_pages/admin_create.js
+++ src/front/js/admin_pages/admin_create.js
@@ -104,31 +104,54 @@ export const AdminCreate = () => {
 		// <div className="mx-auto pt-5">
 		<div className="container-fluid mt-5 pt-5">
 			<div className="container-fluid fixed-top  bg-light" style={{ "margin-top": "59.7px" }}>
-				<div className="p-2 d-flex justify-content-start bg-light">
-					<Button size="sm">
-						<i className="far fa-file-alt" />
-						&nbsp; Texto
-					</Button>
-					&nbsp;
-					<Button size="sm">
-						<i className="fas fa-calculator" />
-						&nbsp; Fórmula
-					</Button>
-					&nbsp;
-					<Button size="sm">
-						<i className="far fa-images" /> &nbsp; Imagen
-					</Button>
-					&nbsp;
-					<Button size="sm">
-						<i className="fas fa-ruler-horizontal" />
-						&nbsp;Separador
-					</Button>
-					&nbsp;
-					<Button size="sm">
-						<i className="fas fa-list" />
-						&nbsp;Lista
-					</Button>
-					&nbsp;
+				<div className="row pt-1">
+					<div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<div className="d-flex justify-content-start bg-light">
+							<Button size="sm">
+								<i className="far fa-file-alt" />
+								&nbsp; Texto
+							</Button>
+							&nbsp;
+							<Button size="sm">
+								<i className="fas fa-calculator" />
+								&nbsp; Fórmula
+							</Button>
+							&nbsp;
+							<Button size="sm">
+								<i className="far fa-images" /> &nbsp; Imagen
+							</Button>
+							&nbsp;
+							<Button size="sm">
+								<i className="fas fa-ruler-horizontal" />
+								&nbsp;Separador
+							</Button>
+							&nbsp;
+							<Button size="sm">
+								<i className="fas fa-list" />
+								&nbsp;Lista
+							</Button>
+							&nbsp;
+						</div>
+					</div>
+					<div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+						<div className="d-flex justify-content-between bg-light">
+							<Button size="sm">
+								<i className="fas fa-sync" />
+								&nbsp; Actualizar
+							</Button>
+							&nbsp;
+							<div>
+								<Button size="sm">
+									<i className="far fa-save" />
+									&nbsp; Guardar
+								</Button>
+								&nbsp;
+								<Button size="sm">
+									<i className="far fa-paper-plane" /> &nbsp; Finalizar
+								</Button>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="row m-2 mt-4 ">
